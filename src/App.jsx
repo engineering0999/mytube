294c7bb683cc234aa23880c5282da2950965d538
engineering0999 <168620473+engineering0999@users.noreply.github.com>
2024-11-10 20:13:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// // src/App.jsx
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Home from './pages/Home';
-// import Search from './pages/Search';
-// import VideoPlayer from './pages/VideoPlayer';
-// import Playlist from './pages/Playlist';
-// import Channel from './pages/Channel';
-// import Test from './pages/test';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/search" element={<Search />} />
-//         <Route path="/test" element={<Test />} />
-//         <Route path="/watch/:videoId" element={<VideoPlayer />} />
-//         <Route path="/playlist/:playlistId" element={<Playlist />} />
-//         <Route path="/channel/:channelId" element={<Channel />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
 // src/App.jsx
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
@@ -37,7 +7,7 @@ import Search from './pages/Search';
 import VideoPlayer from './pages/VideoPlayer';
 import Playlist from './pages/Playlist';
 import Channel from './pages/Channel';
-import Test from './pages/Test';
+import Test from './pages/test';
 import Login from './components/Login';
 import CryptoJS from 'crypto-js';
 
