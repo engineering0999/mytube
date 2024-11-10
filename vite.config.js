f47158c0dd3768116e83d353bd02524de3714663
engineering0999 <168620473+engineering0999@users.noreply.github.com>
2024-11-10 20:38:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: true, // Enable source maps
+    minify: 'terser', // Use Terser for minification
+    terserOptions: {
+      mangle: {
+        properties: {
+          reserved: ['uy'], // Reserve the variable name to avoid renaming
+        },
+      },
+    },
+  },
+});
